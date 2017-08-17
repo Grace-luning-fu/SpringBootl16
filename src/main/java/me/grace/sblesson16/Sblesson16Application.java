979b78b12db85d2ded74e3e193f501e7d0b66e13
Grace-luning-fu <grace.luning.fu@gmail.com>
2017-08-17 09:50:53 -0400

    Initial commit

---
+++ src/main/java/me/grace/sblesson16/Sblesson16Application.java
@@ -0,0 +1,12 @@
+package me.grace.sblesson16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sblesson16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sblesson16Application.class, args);
+	}
+}
